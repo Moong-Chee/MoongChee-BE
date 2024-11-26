0f30b6cbd5c9233d2c44cd9184fd0ba01e8a28f3
huncozyboy <leejh5838@naver.com>
2024-11-27 00:49:41 +0900

    chore: initial commit

---
+++ src/main/java/project/MoongChee/MoongCheeApplication.java
@@ -0,0 +1,13 @@
+package project.MoongChee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongCheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoongCheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/MoongChee/MoongCheeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.MoongChee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoongCheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
