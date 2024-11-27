8d339f606a3bc6be823b7dd36cb22c23e4a0419f
huncozyboy <leejh5838@naver.com>
2024-11-28 04:40:35 +0900

    feat : 유저 예외 메세지 커스텀

---
+++ src/main/java/project/MoongChee/domain/user/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package project.MoongChee.domain.user.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorMessage {
+
+    USER_NOT_FOUND(404, "존재하지 않는 유저입니다."),
+    DUPLICATE_CUSTOMID(400, "중복된 CustomId입니다.");
+
+    private final int code;
+    private final String message;
+
+}
