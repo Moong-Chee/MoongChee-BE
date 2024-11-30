6a38ad1e2e45660c1101368138674a9bcdbcb22a
huncozyboy <leejh5838@naver.com>
2024-11-30 18:32:37 +0900

    refactor : Status 승인 상태 변수명 변경

--- src/main/java/project/MoongChee/domain/user/domain/Status.java
+++ src/main/java/project/MoongChee/domain/user/domain/Status.java
@@ -2,7 +2,7 @@ package project.MoongChee.domain.user.domain;
 
 public enum Status {
     WAITING, // 가입 대기
-    ACTIVE,  // 가입 승인 완료
+    ACCEPTED,  // 가입 승인 완료
     BANNED,  // 정지된 사용자(추후 구현)
     LEFT     // 탈퇴한 사용자(추후 구현)
 }
