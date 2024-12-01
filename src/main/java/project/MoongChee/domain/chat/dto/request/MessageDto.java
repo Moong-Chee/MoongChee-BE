0db9688415baccb97035c3f973bda909ce16734c
huncozyboy <leejh5838@naver.com>
2024-12-01 22:59:11 +0900

    feat : Message 요청 dto 구현

---
+++ src/main/java/project/MoongChee/domain/chat/dto/request/MessageDto.java
@@ -0,0 +1,21 @@
+package project.MoongChee.domain.chat.dto.request;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDto implements Serializable {
+    private static final long serialVersionUID = 2082503192322391880L;
+
+    private Long roomId;
+
+    private Long senderId;
+
+    private String content;
+}
