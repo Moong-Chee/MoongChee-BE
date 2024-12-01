f7bd50620d14e89102251aec055c259392d8a39d
huncozyboy <leejh5838@naver.com>
2024-12-01 22:58:35 +0900

    feat : ChatRoom 도메인 추가

---
+++ src/main/java/project/MoongChee/domain/chat/domain/ChatRoom.java
@@ -0,0 +1,37 @@
+package project.MoongChee.domain.chat.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import project.MoongChee.domain.user.domain.User;
+import project.MoongChee.global.common.domain.BaseTimeEntity;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 접근 레벨 PROTECTED
+@Entity
+public class ChatRoom extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id")
+    private User user1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id")
+    private User user2;
+
+    private String lastMessage;
+}
