e4fe8d9af655457721801af59a174c09923a19ec
1winhyun <jimking1@naver.com>
2024-12-04 12:00:33 +0900

    feat: createdAt 시간이 null로 나오는 오류 해결

--- src/main/java/project/MoongChee/MoongCheeApplication.java
+++ src/main/java/project/MoongChee/MoongCheeApplication.java
@@ -2,12 +2,14 @@ package project.MoongChee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MoongCheeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoongCheeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoongCheeApplication.class, args);
+    }
 
 }
