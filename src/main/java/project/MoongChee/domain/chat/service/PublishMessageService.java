67f6d6645c08d4cd1c9d0f47313637eef7fc6af2
huncozyboy <leejh5838@naver.com>
2024-12-04 18:16:48 +0900

    feat : Pub 서비스 구현

---
+++ src/main/java/project/MoongChee/domain/chat/service/PublishMessageService.java
@@ -0,0 +1,27 @@
+package project.MoongChee.domain.chat.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import project.MoongChee.domain.chat.domain.ChatMessage;
+import project.MoongChee.domain.chat.dto.PublishMessage;
+import project.MoongChee.domain.chat.dto.request.MessageDto;
+import project.MoongChee.domain.chat.redis.RedisPublisher;
+import project.MoongChee.domain.chat.repository.ChatMessageRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PublishMessageService {
+    private final RedisPublisher redisPublisher;
+    private final ChatMessageRepository chatMessageRepository;
+
+    public void publishMessage(MessageDto messageDto) {
+        PublishMessage publishMessage =
+                new PublishMessage(messageDto.getRoomId(), messageDto.getSenderId(), messageDto.getSenderName(),
+                        messageDto.getContent());
+
+        redisPublisher.publish(publishMessage);
+        chatMessageRepository.save(ChatMessage.of(publishMessage));
+    }
+}
