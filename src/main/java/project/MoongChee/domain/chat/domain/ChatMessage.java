56549ca349a51af6dcf5a841211ff896ad7f11f0
huncozyboy <leejh5838@naver.com>
2024-12-04 18:06:08 +0900

    refactor : 채팅 메세지 생성시간 추가

--- src/main/java/project/MoongChee/domain/chat/domain/ChatMessage.java
+++ src/main/java/project/MoongChee/domain/chat/domain/ChatMessage.java
@@ -1,30 +1,38 @@
 package project.MoongChee.domain.chat.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import project.MoongChee.domain.chat.dto.PublishMessage;
-import project.MoongChee.global.common.domain.BaseTimeEntity;
 
-@Document
+@Document(collection = "chatMessage")
 @Getter
 @Builder
-public class ChatMessage extends BaseTimeEntity {
+public class ChatMessage {
     @Id
-    private Long id;
+    private String id;
 
     private Long roomId;
 
     private Long senderId;
 
+    private String senderName;
+
     private String content;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 
     public static ChatMessage of(PublishMessage publishMessage) {
         return ChatMessage.builder()
                 .roomId(publishMessage.getRoomId())
                 .senderId(publishMessage.getSenderId())
+                .senderName(publishMessage.getSenderName())
                 .content(publishMessage.getContent())
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 }
