ff70c771377238298635533974ff1903e8236a8e
huncozyboy <leejh5838@naver.com>
2024-12-04 19:07:38 +0900

    refactor : 채팅방 생성 응답 코드 수정

--- src/main/java/project/MoongChee/domain/chat/controller/ResponseMessage.java
+++ src/main/java/project/MoongChee/domain/chat/controller/ResponseMessage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResponseMessage {
-    CHATROOM_CREATE_SUCCESS(201, "채팅방 생성에 성공했습니다."),
+    CHATROOM_CREATE_SUCCESS(200, "채팅방 생성에 성공했습니다."),
     CHATROOM_GET(200, "하나의 채팅방을 반환합니다."),
     ROOMID_GET(200, "채팅방 번호를 반환합니다."),
     CHATTING_LIST_GET(200, "모든 채팅방 목록을 반환합니다.");
