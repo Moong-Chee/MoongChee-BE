34fcc9500605bc4be7105f8f76ef9bc2527a1e23
huncozyboy <leejh5838@naver.com>
2024-12-04 18:10:25 +0900

    feat : 채팅 컨트롤러 구현

---
+++ src/main/java/project/MoongChee/domain/chat/controller/ChattingController.java
@@ -0,0 +1,42 @@
+package project.MoongChee.domain.chat.controller;
+
+import static project.MoongChee.domain.chat.controller.ResponseMessage.GET_CHATROOM;
+import static project.MoongChee.domain.chat.controller.ResponseMessage.GET_CHATTING_LIST;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.MoongChee.domain.chat.dto.response.ChattingDto;
+import project.MoongChee.domain.chat.dto.response.ChattingListResponseDto;
+import project.MoongChee.domain.chat.service.ChattingService;
+import project.MoongChee.global.common.response.ApiData;
+
+@Tag(name = "CHAT", description = "채팅 컨트롤러")
+@RestController
+@RequestMapping("/api/v1/chat")
+@RequiredArgsConstructor
+public class ChattingController {
+    private final ChattingService chattingService;
+
+    // 채팅방 하나 조회 (대화 내역 반환)
+    @GetMapping("/chatting/{roomId}/{page}/{size}")
+    @Operation(summary = "단일 채팅방 + 대화 내역 조회")
+    public ApiData<ChattingDto> findChatting(@PathVariable Long roomId, @PathVariable Integer page,
+                                             @PathVariable Integer size) {
+        ChattingDto response = chattingService.getChatRoom(roomId, page, size);
+        return ApiData.response(GET_CHATROOM.getCode(), GET_CHATROOM.getMessage(), response);
+    }
+
+
+    @GetMapping("/chattingList/{userId}")
+    @Operation(summary = "특정 유저 모든 채팅방 목록 조회")
+    public ApiData<List<ChattingListResponseDto>> findChattingList(@PathVariable Long userId) {
+        List<ChattingListResponseDto> response = chattingService.getChattingList(userId);
+        return ApiData.response(GET_CHATTING_LIST.getCode(), GET_CHATTING_LIST.getMessage(), response);
+    }
+}
