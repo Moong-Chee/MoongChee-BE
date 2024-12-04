d3191bebbce089163c696f06bf70f34b390884fd
huncozyboy <leejh5838@naver.com>
2024-12-04 18:20:47 +0900

    refactor : 응답 메세지 형식 통일

--- src/main/java/project/MoongChee/domain/chat/controller/ResponseMessage.java
+++ src/main/java/project/MoongChee/domain/chat/controller/ResponseMessage.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
     CHATROOM_CREATE_SUCCESS(201, "채팅방 생성에 성공했습니다."),
-    GET_CHATROOM(200, "하나의 채팅방을 반환합니다."),
-    GET_ROOMID(200, "채팅방 번호를 반환합니다."),
-    GET_CHATTING_LIST(200, "모든 채팅방 목록을 반환합니다.");
+    CHATROOM_GET(200, "하나의 채팅방을 반환합니다."),
+    ROOMID_GET(200, "채팅방 번호를 반환합니다."),
+    CHATTING_LIST_GET(200, "모든 채팅방 목록을 반환합니다.");
 
     private final int code;
     private final String message;
