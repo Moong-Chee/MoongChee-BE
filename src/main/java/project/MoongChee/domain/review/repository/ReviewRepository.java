b9419559637c0b1cafb173ca03e65bd0f75e796b
1winhyun <jimking1@naver.com>
2024-12-05 10:57:33 +0900

    feat: 리뷰 레포지토리 작성

--- src/main/java/project/MoongChee/domain/review/repository/ReviewRepository.java
+++ src/main/java/project/MoongChee/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,7 @@
 package project.MoongChee.domain.review.repository;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.MoongChee.domain.review.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
