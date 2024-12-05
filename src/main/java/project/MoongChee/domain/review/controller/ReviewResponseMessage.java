8751c67ea6a22c67b4ea0624e2b9ea0e2ce72550
1winhyun <jimking1@naver.com>
2024-12-05 11:52:16 +0900

    fix: 리뷰 응답 메시지 코드 변경

--- src/main/java/project/MoongChee/domain/review/controller/ReviewResponseMessage.java
+++ src/main/java/project/MoongChee/domain/review/controller/ReviewResponseMessage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReviewResponseMessage {
-    REVIEW_CREATE_SUCCESS(200, "리뷰 작성 성공"),
+    REVIEW_CREATE_SUCCESS(201, "리뷰 작성 성공"),
     REVIEW_GET_SUCCESS(200, "리뷰 조회 성공");
     private final int code;
     private final String message;
