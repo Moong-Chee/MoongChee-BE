dacbc8c102cb5662778accc812835d78d24336cf
1winhyun <jimking1@naver.com>
2024-12-05 11:18:01 +0900

    feat:리뷰 생성 서비스 작성

--- src/main/java/project/MoongChee/domain/review/dto/ReviewRequestDTO.java
+++ src/main/java/project/MoongChee/domain/review/dto/ReviewRequestDTO.java
@@ -1,8 +1,16 @@
 package project.MoongChee.domain.review.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import project.MoongChee.domain.review.entity.ReviewScore;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewRequestDTO {
     @NotNull ReviewScore reviewScore;
     @NotNull String reviewContent;
--- src/main/java/project/MoongChee/domain/review/service/ReviewService.java
+++ src/main/java/project/MoongChee/domain/review/service/ReviewService.java
@@ -1,4 +1,42 @@
 package project.MoongChee.domain.review.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.MoongChee.domain.post.entity.Post;
+import project.MoongChee.domain.post.exception.PostNotFoundException;
+import project.MoongChee.domain.post.repository.PostRepository;
+import project.MoongChee.domain.review.dto.ReviewRequestDTO;
+import project.MoongChee.domain.review.dto.ReviewResponseDTO;
+import project.MoongChee.domain.review.entity.Review;
+import project.MoongChee.domain.review.repository.ReviewRepository;
+import project.MoongChee.domain.user.domain.User;
+import project.MoongChee.domain.user.service.UserService;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final PostRepository postRepository;
+    private final UserService userService;
+
+    @Transactional
+    public ReviewResponseDTO createReview(Long postId, ReviewRequestDTO requestDTO, String email) {
+        User reviewer = userService.find(email);
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(PostNotFoundException::new);
+
+        User reviewee = post.getAuthor();
+
+        Review review = Review.builder()
+                .reviewScore(requestDTO.getReviewScore())
+                .content(requestDTO.getReviewContent())
+                .reviewer(reviewer)
+                .reviewee(reviewee)
+                .post(post)
+                .build();
+        reviewRepository.save(review);
+        return ReviewResponseDTO.from(review);
+    }
 }
