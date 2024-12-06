4aac8b11d4c077f8cfa1dd751ef0168d66c2e81b
huncozyboy <leejh5838@naver.com>
2024-12-07 01:37:50 +0900

    refactor : 불필요한 에러처리 메세지 삭제

--- src/main/java/project/MoongChee/domain/user/exception/ErrorMessage.java
+++ src/main/java/project/MoongChee/domain/user/exception/ErrorMessage.java
@@ -10,7 +10,6 @@ public enum ErrorMessage {
     USER_NOT_FOUND(404, "존재하지 않는 유저입니다."),
     USER1_NOT_FOUND(404, "유저1은 존재하지 않습니다."),
     USER2_NOT_FOUND(404, "유저2는 존재하지 않습니다."),
-    DUPLICATE_CUSTOMID(400, "중복된 CustomId입니다."),
     INVALID_EMAIL(400, "허용되지 않은 이메일입니다.");
 
     private final int code;
