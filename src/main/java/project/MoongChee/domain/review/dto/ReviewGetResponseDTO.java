bea2555e174238258a675b78351387d49518b7d3
1winhyun <jimking1@naver.com>
2024-12-07 00:24:06 +0900

    fix: reviewCount Integer로 변경

--- src/main/java/project/MoongChee/domain/review/dto/ReviewGetResponseDTO.java
+++ src/main/java/project/MoongChee/domain/review/dto/ReviewGetResponseDTO.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewGetResponseDTO {
-    int reviewCount;
+    Integer reviewCount;
     Double averageScore;
     List<ReviewResponseDTO> reviews;
 }
