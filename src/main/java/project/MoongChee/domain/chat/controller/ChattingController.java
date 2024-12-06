520ce36b846f312363d1199c110412df0d1e6c62
huncozyboy <leejh5838@naver.com>
2024-12-07 01:00:19 +0900

    refactor : 불필요한 주석 삭제

--- src/main/java/project/MoongChee/domain/chat/controller/ChattingController.java
+++ src/main/java/project/MoongChee/domain/chat/controller/ChattingController.java
@@ -23,7 +23,6 @@ import project.MoongChee.global.common.response.ApiData;
 public class ChattingController {
     private final ChattingService chattingService;
 
-    // 채팅방 하나 조회 (대화 내역 반환)
     @GetMapping("/chatting/{roomId}/{page}/{size}")
     @Operation(summary = "단일 채팅방 + 대화 내역 조회")
     public ApiData<ChattingDto> findChatting(@PathVariable Long roomId, @PathVariable Integer page,
@@ -31,7 +30,6 @@ public class ChattingController {
         ChattingDto response = chattingService.getChatRoom(roomId, page, size);
         return ApiData.response(CHATROOM_GET.getCode(), CHATROOM_GET.getMessage(), response);
     }
-
 
     @GetMapping("/chattingList/{userId}")
     @Operation(summary = "특정 유저 모든 채팅방 목록 조회")
