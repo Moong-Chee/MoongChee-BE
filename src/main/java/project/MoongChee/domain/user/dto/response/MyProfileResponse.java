b201fba0ae3ebdff4d58c4b5c40f1575689b250d
huncozyboy <leejh5838@naver.com>
2024-12-06 16:04:51 +0900

    refactor : 내 프로필 조회 응답 dto 수정

---
+++ src/main/java/project/MoongChee/domain/user/dto/response/MyProfileResponse.java
@@ -0,0 +1,33 @@
+package project.MoongChee.domain.user.dto.response;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import project.MoongChee.domain.user.domain.Department;
+import project.MoongChee.domain.user.domain.User;
+
+@Builder
+public record MyProfileResponse(
+        String name,
+        String customId,
+        String profileImageUrl,
+
+        String phoneNumber,
+
+        LocalDate birthday,
+
+        Department department,
+
+        long studentNumber
+) {
+    public static MyProfileResponse from(User user, Department department, long studentNumber, LocalDate birthday) {
+        return MyProfileResponse.builder()
+                .name(user.getName())
+                .profileImageUrl(user.getProfileImage().getUrl())
+                .phoneNumber(user.getPhoneNumber())
+                .birthday(birthday)
+                .department(user.getDepartment())
+                .studentNumber(user.getStudentNumber())
+                .build();
+    }
+}
+
