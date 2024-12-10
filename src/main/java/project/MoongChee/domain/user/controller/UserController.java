474679eeac52e20c2987e21d755d501b5c33286e
huncozyboy <leejh5838@naver.com>
2024-12-11 03:55:01 +0900

    hotfix : Login get 메서드 변경 및 리디렉션 콜백 컨트롤러 추가

--- src/main/java/project/MoongChee/domain/user/controller/UserController.java
+++ src/main/java/project/MoongChee/domain/user/controller/UserController.java
@@ -10,38 +10,49 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import project.MoongChee.domain.user.domain.LoginStatus;
 import project.MoongChee.domain.user.dto.request.InitRequest;
-import project.MoongChee.domain.user.dto.request.SocialLoginRequest;
 import project.MoongChee.domain.user.dto.response.SocialLoginResponse;
-import project.MoongChee.domain.user.service.ProfileService;
 import project.MoongChee.domain.user.service.UserService;
 import project.MoongChee.global.common.response.ApiData;
 
+@Slf4j
 @Tag(name = "USER", description = "유저 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
-    private final ProfileService profileService;
 
     @PostMapping("/login")
     @Operation(summary = "구글 소셜 회원가입 및 로그인")
-    public ApiData<SocialLoginResponse> socialLogin(@RequestBody @Valid SocialLoginRequest request) {
-        SocialLoginResponse response = userService.authenticate(request.authCode());
+    public ApiData<SocialLoginResponse> socialLogin(@RequestParam("code") String authCode) {
+        SocialLoginResponse response = userService.authenticate(authCode);
         if (response.status() == LoginStatus.LOGIN) {
             return ApiData.response(LOGIN_SUCCESS.getCode(), LOGIN_SUCCESS.getMessage(), response);
         }
         return ApiData.response(REGISTER_SUCCESS.getCode(), REGISTER_SUCCESS.getMessage(), response);
+    }
+
+    @GetMapping("/api/v1/users/oauth/google/callback")
+    @Operation(summary = "구글 리디렉션 콜백")
+    public ResponseEntity<String> googleCallback(@RequestParam("code") String code) {
+
+        log.info("Received Google Authorization Code: {}", code);
+
+        // 프론트로 Authorization Code를 전달
+        return ResponseEntity.ok(code);
     }
 
     @PatchMapping("/init")
