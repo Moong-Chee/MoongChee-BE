936e276977da1ab39ecc8947bda04953267c1b55
huncozyboy <leejh5838@naver.com>
2024-12-16 21:34:23 +0900

    refactor : 채팅방 서비스 레디스 내용 삭제

--- src/main/java/project/MoongChee/domain/chat/service/ChatRoomService.java
+++ src/main/java/project/MoongChee/domain/chat/service/ChatRoomService.java
@@ -1,32 +1,38 @@
 package project.MoongChee.domain.chat.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import project.MoongChee.domain.chat.domain.ChatRoom;
 import project.MoongChee.domain.chat.dto.request.CreateChatRoomRequest;
 import project.MoongChee.domain.chat.dto.response.ChatRoomResponse;
+import project.MoongChee.domain.chat.exception.ChatRoomAlreadyExists;
 import project.MoongChee.domain.chat.exception.ChatRoomNotFoundException;
-import project.MoongChee.domain.chat.redis.RedisListener;
 import project.MoongChee.domain.chat.repository.ChatRoomRepository;
 import project.MoongChee.domain.user.domain.User;
-import project.MoongChee.domain.user.service.UserService;
+import project.MoongChee.domain.user.exception.UserNotFoundException;
+import project.MoongChee.domain.user.repository.UserRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
-    private final RedisListener redisMessageListener;
+    private final UserRepository userRepository;
     private final ChatRoomRepository chatRoomRepository;
-    private final UserService userService;
 
     // 채팅방 생성
-    public ChatRoomResponse createChatRoom(CreateChatRoomRequest findChatRoomRequestDto) {
-        User user1 = userService.find(findChatRoomRequestDto.user1Id());
-        User user2 = userService.find(findChatRoomRequestDto.user2Id());
-
-        ChatRoom savedRoom = chatRoomRepository.save(ChatRoom.of(user1, user2));
-        redisMessageListener.adaptMessageListener(savedRoom.getId());
+    public ChatRoomResponse createChatRoom(CreateChatRoomRequest request) {
+        Optional<Long> roomId = chatRoomRepository.findRoomIdByUserIds(request.user1Id(), request.user2Id());
+        if (roomId.isPresent()) {
+            throw new ChatRoomAlreadyExists();
+        }
+        User user1 = userRepository.findById(request.user1Id())
+                .orElseThrow(UserNotFoundException::new);
+        User user2 = userRepository.findById(request.user2Id())
+                .orElseThrow(UserNotFoundException::new);
+        ChatRoom newRoom = ChatRoom.of(user1, user2);
+        ChatRoom savedRoom = chatRoomRepository.save(newRoom);
         return new ChatRoomResponse(savedRoom.getId());
     }
 
